f18b993d0ff9a065117bfc789269ab96bdb27212
alejpaa <alejandro.padillaa@unmsm.edu.pe>
2025-11-19 11:45:39 -0500

    feat(cors): update CORS settings to allow all origins and clean up code fix(requirements): update certifi version and correct package names in requirements

--- backend/app.py
+++ backend/app.py
@@ -4,15 +4,11 @@ from src.api import chat_router
 
 app = FastAPI(title="Ethics Chatbot API", version="1.0.0")
 
-origins = [
-    # Agregar los puertos del frontend
-    "http://localhost:3000",
-    "http://localhost:5173",
-]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins={"*"},
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
