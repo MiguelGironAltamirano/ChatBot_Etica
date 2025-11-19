a02bc37c7ee0b4d5e647299e5821ba3de4f5cd72
alejpaa <alejandro.padillaa@unmsm.edu.pe>
2025-11-19 00:39:04 -0500

    feat(util): update LLM model to gemini-2.5-flash in get_llm_chain

--- backend/src/util/util_llm.py
+++ backend/src/util/util_llm.py
@@ -4,7 +4,7 @@ from src.core.config import settings
 # llm = ChatGoogleGenerativeAI(model="gemini-pro")
 
 get_llm_chain = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-2.5-flash",
     google_api_key=settings.GEMINI_API_KEY,
     temperature=0
 )
